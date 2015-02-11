937133358d9c5a57bf1e6d380df9e14e910fd453
Andy Chu <andychu@google.com>
2015-02-11 08:54:51 -0800

    Add child test

---
+++ apps/api/child_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python -S
+"""
+child_test.py: Tests for child.py
+"""
+
+import unittest
+
+import child  # module under test
+
+
+class FooTest(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def testFoo(self):
+    c = child.Child(
+        ['./pages.R'], input='fifo', output='fifo',
+        # TODO: Move this
+        cwd='.',
+        pgi_version=2,
+        pgi_format='json',
+        )
+    c.Start()
+    # Timeout: Do we need this?  I think we should just use a thread.
+    c.SendHelloAndWait(10.0)
+
+    c.SendHelloAndWait(10.0)
+
+    c.SendRequest({'foo': 'bar'})
+    f = c.OutputStream()
+    resp = f.readline()
+
+    print repr(resp)
+
+
+if __name__ == '__main__':
+  unittest.main()
