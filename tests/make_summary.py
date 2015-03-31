01abdf520f5060fde4df96a33b594e2b0afdca47
Andy Chu <andychu@google.com>
2015-03-31 15:23:59 -0700

    Add summary cell for 3 other columns.

--- tests/make_summary.py
+++ tests/make_summary.py
@@ -46,10 +46,10 @@ SUMMARY_ROW = """\
   <!-- Result metrics -->
   <td></td>
   <td></td>
-  <td></td>
-  <td></td>
+  <td>%(mean_fpf)s</td>
+  <td>%(mean_fnf)s</td>
   <td>%(mean_tv)s</td>
-  <td></td>
+  <td>%(mean_am)s</td>
 </tr>
 </tfoot>
 """
@@ -72,9 +72,9 @@ DETAILS = """\
 """
 
 
-def Percent(n, d):
+def Fraction(n, d):
   """Given numerator and denominator, return a percent string."""
-  return '%.1f%% (%d)' % (float(n) * 100 / d, n)
+  return float(n) / d
 
 
 def main(argv):
@@ -87,6 +87,9 @@ def main(argv):
     test_cases = [line.strip() for line in f]
 
   tv_list = []  # total_variation for all test cases
+  fpf_list = []
+  fnf_list = []
+  am_list = []
 
   for case in test_cases:
     spec = os.path.join(base_dir, case, 'spec.txt')
@@ -105,7 +108,7 @@ def main(argv):
 
     # Format numbers and sum
     (num_actual, num_rappor, num_false_pos, num_false_neg, total_variation,
-     sum_proportion) = metrics_row
+     allocated_mass) = metrics_row
 
     num_actual = int(num_actual)
     num_rappor = int(num_rappor)
@@ -114,23 +117,29 @@ def main(argv):
     num_false_neg = int(num_false_neg)
 
     total_variation = float(total_variation)
-    sum_proportion = float(sum_proportion)
+    allocated_mass = float(allocated_mass)
 
-    tv_list.append(total_variation)
+    # e.g. if there are 20 additional candidates added, and 1 false positive,
+    # the false positive rate is 5%.
+    fp_fraction = Fraction(num_false_pos, num_additional)
+    # e.g. if there are 100 strings in the true input, and 80 strings
+    # detected by RAPPOR, then we have 20 false negatives, and a false
+    # negative rate of 20%.
+    fn_fraction = Fraction(num_false_neg, num_actual)
 
     metrics_row_str = [
         str(num_actual),
         str(num_rappor),
-        # e.g. if there are 20 additional candidates added, and 1 false
-        # positive, the false positive rate is 5%.
-        Percent(num_false_pos, num_additional),
-        # e.g. if there are 100 strings in the true input, and 80 strings
-        # detected by RAPPOR, then we have 20 false negatives, and a false
-        # negative rate of 20%.
-        Percent(num_false_neg, num_actual),
+        '%.1f%% (%d)' % (fp_fraction * 100, num_false_pos),
+        '%.1f%% (%d)' % (fn_fraction * 100, num_false_neg),
         '%.3f' % total_variation,
-        '%.3f' % sum_proportion,
+        '%.3f' % allocated_mass,
     ]
+
+    fpf_list.append(fp_fraction)
+    fnf_list.append(fn_fraction)
+    tv_list.append(total_variation)
+    am_list.append(allocated_mass)
 
     # first cell is test case name, which we already have
     row = spec_row[1:] + metrics_row_str
@@ -143,8 +152,17 @@ def main(argv):
     }
     print ROW % data
 
+  mean_fpf = sum(fpf_list) / len(fpf_list)
+  mean_fnf = sum(fnf_list) / len(fnf_list)
   mean_tv = sum(tv_list) / len(tv_list)
-  summary = {'mean_tv': '%.3f' % mean_tv}
+  mean_am = sum(am_list) / len(am_list)
+
+  summary = {
+      'mean_fpf': '%.1f%%' % (mean_fpf * 100),
+      'mean_fnf': '%.1f%%' % (mean_fnf * 100),
+      'mean_tv': '%.3f' % mean_tv,
+      'mean_am': '%.3f' % mean_am,
+  }
   print SUMMARY_ROW % summary
 
   print '</tbody>'
