f98fc4af7b9168fcf1932dcd4832f81d1ddae822
Ilya Mironov <mironov@gmail.com>
2015-04-03 14:19:01 -0700

    - Addressing review comments. - Changed exponential distribution to geometric, and recentered normal. - Made generation and handling of the 1 0 1 0 1 0 ... vector more idiomatic.

--- tests/gen_counts_test.R
+++ tests/gen_counts_test.R
@@ -18,10 +18,10 @@ library(RUnit)
 
 source('tests/gen_counts.R')
 
-TestProcessAll <- function() {
+TestRandomPartition <- function() {
   
-  p1 <- RandomPartition(total = 100, dexp(1:1000, rate = 5 / 1000))
-  p2 <- RandomPartition(total = 1000, dnorm(1:1000, sd = 1000 / 6))
+  p1 <- RandomPartition(total = 100, dgeom(0:999, prob = .1))
+  p2 <- RandomPartition(total = 1000, dnorm(1:1000, mean = 500, sd = 1000 / 6))
   p3 <- RandomPartition(total = 10000, dunif(1:1000))
   
   # Totals must check out.
@@ -30,23 +30,20 @@ TestProcessAll <- function() {
   checkEqualsNumeric(10000, sum(p3))  
   
   # Initialize the weights vector to 1 0 1 0 1 0 ...
-  weights <- rep(1, 200)
-  even_indices <- 1:100 * 2
-  odd_indices <- even_indices - 1
-  weights[even_indices] <- 0
+  weights <- rep(c(1, 0), 100)
   
   p4 <- RandomPartition(total = 10000, weights)
   
-  # Check that zero weights don't get any mass.
-  checkEqualsNumeric(10000, sum(p4[odd_indices]))
-  checkTrue(all(p4[even_indices] == 0))
+  # Check that all mass is allocated to non-zero weights.
+  checkEqualsNumeric(10000, sum(p4[weights == 1]))
+  checkTrue(all(p4[weights == 0] == 0))
 
   p5 <- RandomPartition(total = 1000000, c(1, 2, 3, 4))
   p.value <- chisq.test(p5, p = c(.1, .2, .3, .4))$p.value
   
   # Apply the chi squared test and fail if p.value is too high or too low.
-  # Probability if failure is 2 * 1E-9, which should never happen.
+  # Probability of failure is 2 * 1E-9, which should never happen.
   checkTrue((p.value > 1E-9) && (p.value <  1 - 1E-9))
 }
 
-TestProcessAll()
+TestRandomPartition()
