e99810740ee4fc9c268aa4c68841f993036a4a4c
Andy Chu <andychu@google.com>
2015-05-02 01:02:42 +0000

    More tweaks to the setup script on a clean machine.

--- apps/gce-setup.sh
+++ apps/gce-setup.sh
@@ -13,13 +13,18 @@ set -o errexit
 native-packages() {
   sudo apt-get update
   # Need build-essential for gcc compilers, invoked while installing R packages.
-  sudo apt-get install build-essential r-base
+  # gfortran needed for glmnet.
+  sudo apt-get install build-essential gfortran r-base
 }
 
 r-packages() {
   # Install as root so you can write to /usr/local/lib/R.
   sudo R -e \
-    'install.packages(c("ggplot2", "glmnet", "optparse", "RUnit"), repos="http://cran.rstudio.com/")'
+    'install.packages(c("glmnet", "optparse", "RUnit"), repos="http://cran.rstudio.com/")'
+
+  # Leaving out ggplot2 for now.  It has the plyr dependency issue on Trusty.
+  # r-cran-plyr is too old.
+  #'install.packages(c("ggplot2"), repos="http://cran.rstudio.com/")'
 }
 
 # Keep Shiny separate, since it seems to install a lot of dependencies.
