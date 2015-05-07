318853d88e7438d0419d5a3e0fcce39412d38334
Andy Chu <andychu@google.com>
2015-05-07 13:24:46 -0700

    Make analysis_tool source() statements consistent.
    It now expects to be run from the repo root.

--- analysis/R/test.sh
+++ analysis/R/test.sh
@@ -11,18 +11,23 @@ set -o pipefail
 set -o errexit
 
 analysis-tool() {
-  local regtest_dir=../../_tmp/regtest
+  # Expects to be run from this dir, and changes to repo root.
+  pushd ../../
+
+  local regtest_dir=_tmp/regtest
 
   mkdir -p _tmp
 
-  # Uses the ./demo.sh run files
-  time ./analysis_tool.R \
+  # Uses the ./demo.sh regtest files
+  time analysis/R/analysis_tool.R \
     --counts $regtest_dir/demo1/1/case_counts.csv \
     --map $regtest_dir/demo1/case_map.csv \
     --config $regtest_dir/demo1/case_params.csv  \
     --output_dir _tmp
 
   cat _tmp/results.csv 
+
+  popd
 }
 
 "$@"
