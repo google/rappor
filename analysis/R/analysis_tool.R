389525d845a8a7f82bf44c6d30a99ab9c79fb035
Andy Chu <andychu@google.com>
2015-07-07 21:45:11 -0700

    Remove unused code in analysis_tool.R.  This will be replaced bya shell script that launches R processes in parallel and creates a nice HTML interface.

--- analysis/R/analysis_tool.R
+++ analysis/R/analysis_tool.R
@@ -1,23 +1,11 @@
 #!/usr/bin/env Rscript
 #
-# IMPORTANT: This script should be deleted when the analysis server is ready.
-#
-# Generates daily Chrome RAPPOR feed. All experiments found in the
-# experiment_config flag are processed for each day.
-#
-# If neither start_date nor end_date are specified, the feed runs on
-# yesterday's data by default. Otherwise, a date range could be indicated
-# to run a backfill.
-#
-# num_days indicates how many days should be aggregated over. If num_days = 7,
-# then 7-day trailing (combining data from the most recent 7 days) analysis for
-# each date (by default, only for yesterday) will be performed. Please use 7
-# days for weekly and 28 days for monthly analyses.
+# Command line tool to decode a RAPPOR data set.  It is a simple wrapper for
+# Decode() in decode.R.
 
 library(optparse)
 library(RJSONIO)
 
-source("analysis/R/analysis_lib.R")
 source("analysis/R/read_input.R")
 source("analysis/R/decode.R")
 source("analysis/R/util.R")
@@ -31,30 +19,15 @@ options(stringsAsFactors = FALSE)
 if (!interactive()) {
   option_list <- list(
     # Flags.
-    make_option("--start_date", default="", help="First date to process. Format: %Y/%m/%d"),
-    make_option("--end_date", default="", help="Last date to process"),
-    make_option("--num_days", default=1, help="Number of trailing days to process"),
-
-    make_option("--experiment_config", default="chrome_rappor_experiments.csv",
-                help="Experiment config file"),
     make_option("--map", default="MA", help="Map file"),
     make_option("--counts", default="CO", help="Counts file"),
     # TODO: Rename this to --params
     make_option("--config", default="", help="Config file"),
+
     make_option("--output_dir", default="./", help="Output directory"),
 
     make_option("--correction", default="FDR", help="Correction method"),
-    make_option("--alpha", default=.05, help="Alpha level"),
-
-    # Input and output directories.
-    make_option("--counts_dir", default="/cns/pa-d/home/chrome-rappor/daily",
-                help="CNS directory where counts files are located"),
-    make_option("--map_dir", default="/cns/pa-d/home/chrome-rappor/map",
-                help="CNS map directory"),
-    make_option("--config_dir", default="/cns/pa-d/home/chrome-rappor/config",
-                help="CNS configuration files directory"),
-    make_option("--release_dir", default="/cns/pa-d/home/chrome-rappor/release",
-                help="Output directory")
+    make_option("--alpha", default=.05, help="Alpha level")
   )
   # NOTE: This API is bad; if you add positional_arguments, the return value changes!
   opts <- parse_args(OptionParser(option_list = option_list))
@@ -99,7 +72,7 @@ ValidateInput <- function(params, counts, map) {
   val
 }
 
-RunOne <- function(opts) {
+main <- function(opts) {
   # Run a single model of all inputs are specified.
   params <- ReadParameterFile(opts$config)
   counts <- ReadCountsFile(opts$counts)
@@ -153,148 +126,6 @@ RunOne <- function(opts) {
       res$metrics$num_detected, res$metrics$allocated_mass)
 
   Log('DONE')
-}
-
-# Run multiple models.  There is a CSV experiments config file, and we invoke
-# AnalyzeRAPPOR once for each row in it.
-RunMany <- function(opts) {
-
-  # If the date is not specified, run yesterday's analyses only.
-  if (opts$start_date == "" && opts$end_date == "") {
-    start_date <- Sys.Date() - 1
-    end_date <- start_date
-  } else {
-    start_date <- as.Date(opts$start_date)
-    end_date <- as.Date(opts$end_date)
-    if (end_date < start_date) {
-      stop("End date should be larger or equal than start date!")
-    }
-  }
-  dates <- as.character(seq(start_date, end_date, 1))
-
-  # List of experiments to analyze.
-  config_path = file.path(opts$config_dir, opts$experiment_config)
-  Log('Reading experiment config %s', config_path)
-  experiments <- read.csv(config_path,
-                          header = FALSE, as.is = TRUE,
-                          colClasses = "character", comment.char = "#")
-
-  for (date in dates) {
-    Log("Date: %s", date)
-    date <- as.Date(date)
-    year <- format(date, "%Y")
-    month <- format(date, "%m")
-    day <- format(date, "%d")
-
-    # Create an output directory.
-    output_dir <- file.path(opts$release_dir, year, month, day)
-    dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-
-    base_filename <- "chrome_rappor_experiments_"
-    output_filename <-
-      switch(as.character(opts$num_days),
-             "1" = paste0(base_filename, date, ".csv"),
-             "7" = paste0("weekly_", base_filename, date, ".csv"),
-             "28" = paste0("monthly_", base_filename, date, ".csv"),
-             paste0(opts$num_days, "_", base_filename, date, ".csv"))
-
-    # Delete any existing files with the same name.
-    unlink(file.path(output_dir, output_filename))
-
-    res <- vector("list", nrow(experiments))
-    for (i in 1:nrow(experiments)) {
-      cat(paste0("Experiment ", i, " (of ",
-                 nrow(experiments), "): ", experiments[i, 2], "\n"))
-
-      # Process a line in the experiments file.
-      experiment_name <- experiments[i, 2]
-      map_file <- experiments[i, 3]
-      config_file <- experiments[i, 4]
-
-      # Read in input files specified in the experiments file.
-      params_path = file.path(opts$config_dir, config_file)
-      Log('Reading params %s', params_path)
-      config <- ReadParameterFile(params_path)
-
-      map_path <- file.path(opts$map_dir, map_file)
-      Log('Loading map %s', map_path)
-      LoadMapFile(map_path)  # Loads the "map" object.
-
-      # Read one or more counts file.
-      counts_file <- paste0(experiments[i, 1], "_counts.csv")
-      trailing_dates <- as.character(seq(date - opts$num_days + 1, date, 1))
-      counts_list <- list()
-      for (j in 1:length(trailing_dates)) {
-        counts_path = file.path(opts$counts_dir, trailing_dates[j], counts_file)
-        Log("Reading counts %s", counts_path)
-
-        counts_j <- ReadCountsFile(file.path(opts$counts_dir,
-                                             trailing_dates[j], counts_file))
-        if (!is.null(counts_j)) {
-          counts_list[[j]] <- AdjustCounts(counts_j, config)
-        }
-      }
-      counts <- Reduce("+", counts_list)  # Turn list into matrix
-
-      # Perform the analysis.
-
-      Log("CONFIG")
-      str(config)
-      cat('\n')
-
-      Log("COUNTS")
-      str(counts)
-      cat('\n')
-
-      Log("MAP")
-      str(map$map)
-      cat('\n')
-
-      exp_res <- AnalyzeRAPPOR(config, counts, map$map, opts$correction, opts$alpha,
-                               experiment_name = experiment_name,
-                               map_name = map_file,
-                               config_name = config_file,
-                               date = as.character(date),
-                               date_num = as.numeric(format(date, "%Y%m%d")))
-
-      if (!is.null(exp_res)) {
-        res[[i]] <- exp_res
-        cat("Discovered:", nrow(exp_res), "\n\n")
-      } else {
-        cat("Discovered: 0\n\n")
-      }
-    }
-
-    # Write out a single column IO file for each date.
-    output <- do.call("rbind", res)
-    if (!is.null(output) && nrow(output) > 0) {
-      path = file.path(output_dir, output_filename)
-      write.csv(output, path)
-      Log('Wrote %s', path)
-    }
-
-    Log("RES")
-    str(res)
-    cat('\n')
-
-    Log("OUTPUT")
-    str(output)
-    cat('\n')
-
-    Log("sum(proportion)")
-    print(sum(output$proportion))
-
-    Log("sum(estimate)")
-    print(sum(output$estimate))
-  }
-}
-
-main = function(opts) {
-  if (opts$counts != "" && opts$map != "" && opts$config != "") {
-    RunOne(opts)
-  } else {
-    RunMany(opts)
-  }
 }
 
 if (!interactive()) {
