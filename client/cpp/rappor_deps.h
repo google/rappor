9bb1cc0fc2a1262f82a38097b43d78b16b099596
Andy Chu <andychu@google.com>
2015-07-22 21:57:23 -0700

    Cleanup

--- client/cpp/rappor_deps.h
+++ client/cpp/rappor_deps.h
@@ -21,7 +21,7 @@
 namespace rappor {
 
 // rappor::Bits type is used for Bloom Filter, PRR, and IRR
-typedef uint64_t Bits; 
+typedef uint64_t Bits;
 
 // NOTE: If using C++11 (-std=c++0x), it's safer to do this:
 // typedef std::array<unsigned char, 32> Sha256Digest;
@@ -31,26 +31,20 @@ typedef unsigned char Sha256Digest[32];
 
 // rappor:Encoder needs an MD5 function for the bloom filter, and an HMAC
 // function for the PRR.
-//
-// And a random function for the IRR.
-//   NOTE: libc rand returns a float between 0 and 1.
-// Maybe you just need to return p_bits and q_bits.
 
-// NOTE: is md5 always sufficient?  Maybe you should have a generic hash.
-// string -> string?  But you need to know how many bits there are.
-// num_hashes * log2(num_bits) == 2 * log2(8) = 6, or 2 * log2(128) = 14.
-//
-// TODO: Do these really fail?  I don't think md5 does.
+// TODO: Make hash generic, and allow failure?
 
 typedef bool Md5Func(const std::string& value, Md5Digest output);
 typedef bool HmacFunc(const std::string& key, const std::string& value,
                       Sha256Digest output);
 
-// Interface that the encoder requires.  Applications should implement this
-// based on their platform and requirements.
+// Interface that the encoder use to generate randomness for the IRR.
+// Applications should implement this based on their platform and requirements.
 class IrrRandInterface {
  public:
   virtual ~IrrRandInterface() {}
+  // Compute a bitmask with each bit set to 1 with probability 'prob'.
+  // Returns false if there is an error.
   virtual bool GetMask(float prob, int num_bits, Bits* mask_out) const = 0;
 };
 
