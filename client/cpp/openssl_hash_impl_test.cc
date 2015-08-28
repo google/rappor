8702610fef895005689d36bb4364f881c57e7b2a
Andy Chu <andychu@google.com>
2015-08-28 15:31:27 -0700

    Simplify test

--- client/cpp/openssl_hash_impl_test.cc
+++ client/cpp/openssl_hash_impl_test.cc
@@ -1,4 +1,7 @@
+#include <stdint.h>  // for uint32_t
 #include <stdio.h>
+#include <string>
+#include <vector>
 
 #include "openssl_hash_impl.h"
 
@@ -7,48 +10,24 @@
 int main() {
   std::string key("key");
   std::string value("value");
-  rappor::Sha256Digest sha256;
+  std::vector<uint8_t> sha256;
 
-  bool result = rappor::Hmac(key, value, sha256);
-  printf("result: %d\n", result);
+  bool ok1 = rappor::Hmac(key, value, &sha256);
+  printf("ok: %d\n", ok1);
   printf("digest:\n");
 
-  const int n = sizeof(sha256);
-  for (int i = 0; i < n; ++i) {
+  for (size_t i = 0; i < sha256.size(); ++i) {
     printf("%02x", sha256[i]);
   }
   printf("\n");
 
-  rappor::Md5Digest md5;
+  std::vector<uint8_t> md5;
 
-  bool ok = rappor::Md5(value, md5);
-  printf("ok: %d\n", ok);
+  bool ok2 = rappor::Md5(value, &md5);
+  printf("ok: %d\n", ok2);
 
-  for (size_t i = 0; i < sizeof(md5); ++i) {
+  for (size_t i = 0; i < md5.size(); ++i) {
     printf("%02x", md5[i]);
   }
   printf("\n");
-
-  // how long are secrets?  Probably should be reasonably long
-  // ~300 ms for 1M.
-  // So then that's 300 ns.  Fast.
-  //for (int i = 0; i < 1000000; ++i) {
-  //  bool ok = rappor::Md5("01234567890123456789", md5);
-  //}
-
-  // 3 seconds for this.  So that's 3 us per HMAC value.
-  //
-  // For simulation, you can just use 1 byte secrets, so I guess simulation
-  // speed isn't really an issue.
-
-  std::string key2("01234567890123456789");
-  // It's not that much faster with this small key -- 2.8 seconds.
-  //std::string key2("0");
-
-  std::string value2("01234567890123456789");
-  //std::string value2("0");
-
-  for (int i = 0; i < 1000000; ++i) {
-    rappor::Hmac(key2, value2, sha256);
-  }
 }
