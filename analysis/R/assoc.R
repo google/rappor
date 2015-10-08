d1b5ba461f1d8f9ececdb790755b9291ae9b0cb8
Ananth Raghunathan <pseudorandom@google.com>
2015-10-08 11:13:16 -0700

    Commiting changes locally.

--- analysis/R/assoc.R
+++ analysis/R/assoc.R
@@ -15,10 +15,8 @@
 # limitations under the License.
 
 # Reads map files, report files, and RAPPOR parameters to run
-# an EM algorithm to estimate joint distribution over two or more variables
-#
-# NOTE: Also has code for the 2 way algorithm but the code is still
-# experimental and not checked in completely.
+# an EM algorithm to estimate joint distribution over two variables one of
+# which is boolean (or Basic Rappor)
 #
 # Usage:
 #       $ ./assoc.R --inp <JSON file>
@@ -47,149 +45,17 @@ if(!interactive()) {
   opts <- parse_args(OptionParser(option_list = option_list))
 }
 
-# Currently disabled. 2 Way algorithm will not run.
-# source("analysis/R/decode2way.R")
-source("analysis/R/encode.R")
+# source("analysis/R/encode.R")
 source("analysis/R/decode.R")
-source("analysis/R/simulation.R")
+# source("analysis/R/simulation.R")
 source("analysis/R/read_input.R")
 source("analysis/R/association.R")
-source("tests/gen_counts.R")
+# source("tests/gen_counts.R")
 
-# Analysis where second variable is basic RAPPOR
-TwoWayAlgBasic <- function(inp) {
-  ptm <- proc.time()
-  params <- ReadParameterFileMulti(inp$params)
-  stopifnot(inp$numvars == length(inp$maps) ||
-              inp$numvars == length(params))
-  map <- CorrectMapForAssoc(ReadMapFile(inp$maps[[1]], params = params[[1]]),
-                            params = params[[1]])
-  
-  # 2 way counts, marginal 1 counts, marginal 2 counts
-  counts <- lapply(1:3, function(i) ReadCountsFile(inp$counts[[i]]))
-  
-  # Prune candidates for variable 1
-  fit <- Decode(counts[[2]], map$rmap, params[[1]], quick = FALSE)$fit
-  print(fit)
-  found_strings <- fit[,"string"]
-  
-  if(length(found_strings) == 0)
-    stop("No strings found in 1-way marginal.")
-  
-  pruned_map <- lapply(map$map, function(z) z[,found_strings, drop = FALSE])
-  map2 <- lapply(1:length(pruned_map), function(z) {
-    m <- sparseMatrix(c(1), c(2), dims = c(1, 2))
-    colnames(m) <- c("FALSE", "TRUE")
-    m
-  })
-  # Final map
-  fmap <- CombineMaps(pruned_map, map2)$crmap
-  params2 <- params[[1]]
-  # Only for boolean
-  params2$k <- (params[[1]]$k) * 4
-  # Get true values (for debug purposes only)
-  truevals <- read.csv(inp$truevals)[,c("value1", "value2")]
-  truevals <- table(truevals) / sum(table(truevals))
-  colnames(truevals) <- c("FALSE", "TRUE")
-  marginal <- Decode2Way(counts[[1]], fmap, params2,
-                         fit = fit[,c("string", "proportion")])$fit
-  rs <- rowSums(truevals)
-  fits <- data.frame(string = rownames(as.data.frame(rs)),
-                     proportion = as.vector(rs))
-  truecol = NULL
-  for (rows in found_strings) {
-    for (cols in c("FALSE", "TRUE")) {
-      truecol <- c(truecol, truevals[rows, cols])
-    }
-  }
-  marginal <- cbind(marginal, true = truecol)
-  for (i in -3:3) {
-    fits_t <- fits
-    fits_t[,"proportion"] <- fits_t[,"proportion"] * (1 + i/10)
-    marginal <- cbind(marginal,
-                      more = Decode2Way(counts[[1]],
-                                 fmap,
-                                 params2,
-                                 fit = fits_t)$fit[,"Estimate"])
-    print("ABS")
-    print(0.5 * sum(abs(marginal$true-marginal[,i + 8])))
-  }
-  ed <- matrix(0, nrow = length(found_strings), ncol = 2)
-  colnames(ed) <- c("FALSE", "TRUE")
-  rownames(ed) <- found_strings
-  for (rows in rownames(ed)) {
-    for (cols in colnames(ed)) {
-      ed[rows, cols] <- marginal[paste(rows, cols, sep = "x"), "Estimate"] 
-    }
-  }
-
-  print(marginal)
-  print(sum(marginal[,"Estimate"]))
-  ed[is.na(ed)] <- 0
-  ed[ed<0] <- 0
-  
-  time_taken <- proc.time() - ptm
-  print("Two Way Algorithm Results")
-  # print(ed)
-  # print(ed[order(-rowSums(ed)), order(-colSums(ed))])
-}
-
-TwoWayAlg <- function(inp) {
-  ptm <- proc.time()
-  params <- ReadParameterFile(inp$params)
-  # Ensure sufficient maps as required by number of vars
-  # Correct map from ReadMapFile() for assoc analysis
-  stopifnot(inp$numvars == length(inp$maps))
-  map <- lapply(inp$maps, function(o)
-    CorrectMapForAssoc(ReadMapFile(o, params = params),
-                       params = params))
-  
-  # (2 way counts, marginal 1 counts, marginal 2 counts)
-  counts <- lapply(1:3, function(i) ReadCountsFile(inp$counts[[i]]))
-  
-  # TODO: account for different parameters across different variables
-  params2 <- params
-  params2$k <- (params$k ** 2) * 4
-  
-  # Prune candidates
-  fit <- lapply(1:2, function(i)
-    Decode(counts[[i + 1]],
-           map[[i]]$rmap,
-           params, quick = FALSE)$fit)
-  
-  found_strings = list(fit[[1]][,"string"], fit[[2]][,"string"])
-  
-  if (length(found_strings[[1]]) == 0 || length(found_strings[[2]]) == 0) {
-    stop("No strings found in 1-way marginal.")
-  }
-  
-  # Combine maps to feed into Decode2Way
-  # Prune first to found_strings from Decode on 1-way counts
-  pruned <- lapply(1:2, function(i)
-    lapply(map[[i]]$map, function(z) z[,found_strings[[i]], drop = FALSE]))
-  crmap <- CombineMaps(pruned[[1]], pruned[[2]])$crmap
-  marginal <- Decode2Way(counts[[1]], crmap, params2, fit = fit)$fit
-  
-  # Reconstruct 2-way table from marginals
-  ed <- matrix(0, nrow = length(found_strings[[1]]), ncol = length(found_strings[[2]]))
-  colnames(ed) <- found_strings[[2]]
-  rownames(ed) <- found_strings[[1]]
-  for (cols in found_strings[[2]]) {
-    for (rows in found_strings[[1]]) {
-      ed[rows, cols] <- marginal[paste(rows, cols, sep = "x"), "Estimate"]
-    }
-  }
-  ed[is.na(ed)] <- 0
-  ed[ed<0] <- 0
-  
-  time_taken <- proc.time() - ptm
-  print("Two Way Algorithm Results")
-  print(ed[order(-rowSums(ed)), order(-colSums(ed))])
-  if(inp$time == TRUE)
-    print(time_taken)
-}
-
-EMAlg <- function(inp) {
+# Implements the EM algorithm on two variables
+# NOTE: var 2 assumed to be reported via Basic RAPPOR
+# TODO(pseudorandom): Remove this assumption on var 2.
+EMAlgBasic <- function(inp) {
   ptm <- proc.time()
   params <- ReadParameterFile(inp$params)
   # Ensure sufficient maps as required by number of vars
@@ -199,7 +65,11 @@ EMAlg <- function(inp) {
     CorrectMapForAssoc(LoadMapFile(o, params = params),
                        params = params))
   
-  # For BASIC only
+  # For Basic rappor, we need to setup a map file manually for var 2
+  # The map file has 2 components: 
+  # - an array of simple (0, 1) map to represent Basic rappor values for each
+  # cohort
+  # - a combined map (rmap) that collapses cohorts
   m1 <- lapply(1:params$m, function(z) {
     m <- sparseMatrix(c(1), c(2), dims = c(1, 2))
     colnames(m) <- c("FALSE", "TRUE")
@@ -212,28 +82,34 @@ EMAlg <- function(inp) {
   
   # Reports must be of the format
   #     client name, cohort no, rappor bitstring 1, rappor bitstring 2, ...
-  reportsObj <- read.csv(inp$reports,
-                         colClasses = c("character", "integer",
-                                        rep("character", inp$numvars)),
-                         header = TRUE)
+  # Reading to temporary data frame
+  tmp_reports_df <- read.csv(inp$reports,
+                             colClasses = c("character", "integer",
+                                            rep("character", inp$numvars)),
+                             header = TRUE)
   # Ignore the first column
-  reportsObj <- reportsObj[,-1]
+  tmp_reports_df <- tmp_reports_df[,-1]
   
+  # TODO(pseudorandom): Do not assume var 2 is always boolean
+  # Fix which params$k is set to 1
   params = list(params, params)
   params[[2]]$k = 1
   
-  # Parsing reportsObj
+  # Parsing tmp_reports_df
   # ComputeDistributionEM allows for different sets of cohorts
   # for each variable. Here, both sets of cohorts are identical
-  co <- as.list(reportsObj[1])[[1]]
-  co <- co + 1  # 1 indexing
-  cohorts <- rep(list(co), inp$numvars)
-  # Parse reports from reportObj cols 2, 3, ...
-  reports <- lapply(1:inp$numvars, function(x) as.list(reportsObj[x + 1]))
+  tmp_cohorts <- as.list(tmp_reports_df[1])$cohort
+  tmp_cohorts <- tmp_cohorts + 1  # 1 indexing for EM algorithm
+  # Expanded list of report cohorts
+  cohorts <- rep(list(tmp_cohorts), inp$numvars)
+  # Parse reports from tmp_reports_df cols 2, 3, ...
+  # into a reports array
+  reports <- lapply(1:inp$numvars, function(x) as.list(tmp_reports_df[x + 1]))
   
-  # Split strings into bit arrays (as required by assoc analysis)
+  # Split ASCII strings into array of numerics (as required by assoc analysis)
   reports <- lapply(1:inp$numvars, function(i) {
     # apply the following function to each of reports[[1]] and reports[[2]]
+    # TODO(pseudorandom): Do not assume var 2 is always boolean
     lapply(reports[[i]][[1]], function(x) {
       # function splits strings and converts them to numeric values
       # rev needed for endianness
@@ -241,6 +117,11 @@ EMAlg <- function(inp) {
     })
   })
   
+  # ignore_other = FALSE because in doing association with Basic RAPPOR, we're
+  # going to use the Other category to estimate no. of reports where the bit
+  # is unset
+  # marginals and estimate_var disabled
+  # verbose outputs timing information for efficiency analysis; set to inp$time
   joint_dist <- ComputeDistributionEM(reports, cohorts, map,
                                       ignore_other = FALSE,
                                       params, marginals = NULL,
@@ -248,7 +129,10 @@ EMAlg <- function(inp) {
                                       verbose = inp$time)
   em <- joint_dist$fit
   time_taken <- proc.time() - ptm
-  # Replace Other column name with FALSE
+  cat("Came here \n\n")
+  # Replace Other column name with FALSE. In Basic RAPPOR we use Other
+  # category to estimate no. of reports with unset bits (which are essentially
+  # FALSE)
   colnames(em)[which(colnames(em) == "Other")] <- "FALSE"
   
   # Unroll and write to results.csv
@@ -266,11 +150,11 @@ EMAlg <- function(inp) {
 main <- function(opts) {
   inp <- fromJSON(opts$inp)
   if(inp$algo == "EM")
-    EMAlg(inp)
+    EMAlgBasic(inp)
   # Recommendation: Use EMAlg; TwoWayAlgBasic is still experimental
   # and code is not completely checked in yet.
-  if(inp$algo == "2Way")
-    TwoWayAlgBasic(inp)
+  # if(inp$algo == "2Way")
+  #   TwoWayAlgBasic(inp)
 }
 
 if(!interactive()) {
