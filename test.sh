d949bdb7edd089fdfd0b1641663ea58c6418c60c
Hamid Ebadi <hamid.ebadi@gmail.com>
2016-08-24 12:02:21 +0200

    Fixing some minor bugs

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # Copyright 2014 Google Inc. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -87,7 +87,7 @@ lint() {
   banner "Linting Python source files"
   py-lint
   echo
-  
+
   banner "Linting Documentation files"
   doc-lint
 }
@@ -126,12 +126,12 @@ r-unit() {
   tests/compare_dist_test.R
 
   tests/gen_counts_test.R
-  
+
   tests/gen_true_values_test.R
 
   analysis/R/decode_test.R
 
-  analysis/test/run_tests.R
+  analysis/R/run_tests.R
 }
 
 doc-lint() {
